ccc6b0d3a030df4f671f9b7ce23dd7f419eea8a4
qiyizhong <qiyizhong@corp.netease.com>
2019-02-18 15:33:07 +0800

    [Medium]Range Sum of BST补充栈的方式

--- swift-leetcode/Medium/Range Sum of BST.swift
+++ swift-leetcode/Medium/Range Sum of BST.swift
@@ -19,23 +19,65 @@ public class TreeNode {
     }
 }
 
-var sum = 0
-var tempL = 0
-var tempR = 0
+// MARK: 栈
+public struct Stack<T> {
+    fileprivate var array = [T]()
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    public var count: Int {
+        return array.count
+    }
+    public mutating func push(_ element: T) {
+        array.append(element)
+    }
+    public mutating func pop() -> T? {
+        return array.popLast()
+    }
+    public var top: T? {
+        return array.last
+    }
+}
 
 func rangeSumBST(_ root: TreeNode?, _ L: Int, _ R: Int) -> Int {
-    tempL = L
-    tempR = R
-    searchBTS(root)
+    var sum = 0
+    var stack: Stack<TreeNode> = Stack()
+    if root != nil {
+        stack.push(root!)
+    }
+    while stack.count > 0 {
+        let node = stack.pop()
+        if (node?.val)! >= L && (node?.val)! <= R {
+            sum += (node?.val)!
+        }
+        if node?.left != nil {
+            stack.push((node?.left)!)
+        }
+        if node?.right != nil {
+            stack.push((node?.right)!)
+        }
+    }
     return sum
 }
 
-func searchBTS(_ root: TreeNode?) {
-    if root != nil {
-        if root!.val >= tempL && root!.val <= tempR {
-            sum += (root?.val)!
-        }
-        searchBTS(root?.left)
-        searchBTS(root?.right)
-    }
-}
+// MARK: 递归
+//var sum = 0
+//var tempL = 0
+//var tempR = 0
+//
+//func rangeSumBST(_ root: TreeNode?, _ L: Int, _ R: Int) -> Int {
+//    tempL = L
+//    tempR = R
+//    searchBTS(root)
+//    return sum
+//}
+//
+//func searchBTS(_ root: TreeNode?) {
+//    if root != nil {
+//        if root!.val >= tempL && root!.val <= tempR {
+//            sum += (root?.val)!
+//        }
+//        searchBTS(root?.left)
+//        searchBTS(root?.right)
+//    }
+//}
