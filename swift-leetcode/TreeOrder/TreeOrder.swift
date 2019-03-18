277062c61f8ad38fb9b45ca8caae629a201c0286
Q YiZhong <178159283@qq.com>
2019-03-18 16:16:22 +0800

    [二叉树]层序遍历

---
+++ swift-leetcode/TreeOrder/TreeOrder.swift
@@ -0,0 +1,37 @@
+//
+//  TreeOrder.swift
+//  swift-leetcode
+//
+//  Created by Q YiZhong on 2019/3/18.
+//  Copyright © 2019 YiZhong Qi. All rights reserved.
+//
+
+import Foundation
+
+//let tree = TreeNode.init(0)
+//tree.left = TreeNode.init(1)
+//tree.right = TreeNode.init(2)
+//tree.left?.left = TreeNode.init(3)
+//tree.left?.right = TreeNode.init(4)
+//tree.right?.left = TreeNode.init(5)
+//tree.right?.right = TreeNode.init(6)
+
+// 层序遍历
+func levelOrder(_ head: TreeNode?) {
+    guard head != nil else {
+        return
+    }
+    var queue: [TreeNode] = []
+    queue.append(head!)
+    while queue.count != 0 {
+        let front = queue[0]
+        queue.remove(at: 0)
+        if front.left != nil {
+            queue.append(front.left!)
+        }
+        if front.right != nil {
+            queue.append(front.right!)
+        }
+        print(front.val)
+    }
+}
