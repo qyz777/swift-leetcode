01e1661b1e2ebe56ace3dc8645c6698cd205dafb
Q YiZhong <178159283@qq.com>
2019-04-09 21:44:58 +0800

    [Medium][BFS]200 岛屿的个数

---
+++ swift-leetcode/BFS/Medium/200.swift
@@ -0,0 +1,40 @@
+//
+//  200.swift
+//  swift-leetcode
+//
+//  Created by Q YiZhong on 2019/4/9.
+//  Copyright © 2019 YiZhong Qi. All rights reserved.
+//
+
+import Foundation
+
+func numIslands(_ grid: [[Character]]) -> Int {
+    func _BFS(_ grid: inout [[Character]], _ x: Int, _ y: Int) {
+        guard x < grid.count && y < grid[0].count else {
+            return
+        }
+        grid[x][y] = Character("e")
+        let X = [-1, 1, 0, 0]
+        let Y = [0, 0, -1, 1]
+        for i in 0..<4 {
+            let mX = x + X[i]
+            let mY = y + Y[i]
+            if (mX >= 0 && mX < grid.count) && (mY >= 0 && mY < grid[0].count) {
+                if grid[mX][mY] == "1" {
+                    _BFS(&grid, mX, mY)
+                }
+            }
+        }
+    }
+    var g = grid
+    var count = 0
+    for i in 0..<g.count {
+        for j in 0..<g[0].count {
+            if g[i][j] == "1" {
+                count += 1
+                _BFS(&g, i, j)
+            }
+        }
+    }
+    return count
+}
