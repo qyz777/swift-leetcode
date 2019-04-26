d668000949fbb29de86953ba0f38a27ef7d9eb09
Q YiZhong <178159283@qq.com>
2019-04-26 18:29:56 +0800

    [辅助][工程]层序遍历构建二叉树

--- swift-leetcode/TreeNode.swift
+++ swift-leetcode/TreeNode.swift
@@ -17,4 +17,29 @@ public class TreeNode {
         self.left = nil
         self.right = nil
     }
+    
+    public class func generateTree(_ nums: [Int?]) -> TreeNode? {
+        guard nums.count > 0 else {
+            return nil
+        }
+        var nums = nums
+        let root = TreeNode.init(nums.removeFirst() ?? 0)
+        var queue: [TreeNode] = [root]
+        while !nums.isEmpty {
+            var tempQueue: [TreeNode] = []
+            while !queue.isEmpty {
+                let p = queue.removeFirst()
+                if !nums.isEmpty, let left = nums.removeFirst() {
+                    p.left = TreeNode.init(left)
+                    tempQueue.append(p.left!)
+                }
+                if !nums.isEmpty, let right = nums.removeFirst() {
+                    p.right = TreeNode.init(right)
+                    tempQueue.append(p.right!)
+                }
+            }
+            queue = tempQueue
+        }
+        return root
+    }
 }
